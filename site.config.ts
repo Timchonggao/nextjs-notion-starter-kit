09cb7b9524d3b18916d8d16a270560c28cb69120
Timchonggao <104890025+Timchonggao@users.noreply.github.com>
2022-08-11 10:07:10 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8bf3b967d2a94924b258e727030e6208',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
+  
+  
+  
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Getting Started',
+  domain: 'cool-bottom-f3e',
+  author: 'Chong Gao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
